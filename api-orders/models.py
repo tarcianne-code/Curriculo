dab172edd5816fdf02219712192596475e89b32c
Tarcianne Rocha <tarciannesrocha1@gmail.com>
2026-04-24 13:36:59 -0300

    Create models.py

---
+++ api-orders/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True, index=True)
+    product = Column(String)
+    status = Column(String, default="pendente")
